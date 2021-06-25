97a46cf430a661d39c77c57921dc8d1607be520b
ananduv2 <ananduv2.0@gmail.com>
2021-06-25 22:12:01 +0530

    Options added for department

--- accounts/models.py
+++ accounts/models.py
@@ -3,13 +3,23 @@ import datetime
 
 # Create your models here.
 
+DEPARTMENT=(
+    ('INTERNS','INTERNS'),
+    ('BACKEND_DEVELOPER','BACKEND_DEVELOPER'),
+    ('FRONTEND_DEVELOPER','FRONTEND_DEVELOPER'),
+    ('DESIGNER','DESIGNER'),
+    ('HR','HR'),
+    ('MARKETING','MARKETING'),
+    ('ADMINISTRATOR','ADMINISTRATOR')
+)
+
 class employee(models.Model):
     name =models.CharField(max_length=100)
     email = models.EmailField(max_length=300)
     mobile=models.IntegerField(null=True)
     dob=models.DateField(null=True)
     doj=models.DateField(default=datetime.date.today)
-    department=models.CharField(max_length=50,null=True)
+    department=models.CharField(max_length=50,choices=DEPARTMENT,default="INTERNS")
 
 
     def __str__(self):
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
+from models import employee
 
 # Create your views here.
